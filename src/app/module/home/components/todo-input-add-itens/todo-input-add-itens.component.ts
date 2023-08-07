2663778be552fb7896278e9b4ab9f1e95a488b3a
luxyvsc <pedro.tostessilva1@gmail.com>
2023-08-07 17:35:28 -0300

    first commit

---
+++ src/app/module/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-input-add-itens',
+  templateUrl: './todo-input-add-itens.component.html',
+  styleUrls: ['./todo-input-add-itens.component.scss']
+})
+export class TodoInputAddItensComponent {
+  @Output() public emmitItemTaskList = new EventEmitter<string>();
+
+  public addItemTaskList: string = "";
+
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim()
+    if(this.addItemTaskList){
+      this.emmitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }  
+  }
+}
---
+++ src/app/module/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,53 @@
+import { Component, DoCheck } from '@angular/core';
+
+//interface
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements DoCheck {
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
+
+
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
+
+  public deleteItemTaskList(event: number) { 
+    this.taskList.splice(event, 1)
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja deletar a lista?")
+    if(confirm){
+      this.taskList = [];
+    }
+  }
+
+  public newItemTask(value: string) {
+    this.taskList.push({task: value, checked: false});
+  }
+
+  public validationTask(event: string, i: number) {
+    if(!event.length) {
+      const confirm = window.confirm("você deseja apagar essa task?");
+      if(confirm){
+        this.deleteItemTaskList(i);
+      }
+    }else {
+      console.log(i)
+    }
+
+  }
+
+  public setLocalStorage() {
+    if(this.taskList){
+      this.taskList.sort( (first, last) =>  Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
+  }
+
+}
